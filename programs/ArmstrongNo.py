782657b44f15d60edb5c70282129c69ed380312d
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-06-01 12:16:39 +0530

    check if given no is Armstrong or not

---
+++ programs/ArmstrongNo.py
@@ -0,0 +1,23 @@
+# check if given no. is armstrong no.(also called as narcissistic no/plus perfect no.)-
+# 153,371,9474 are narcissistic no.
+# Plus perfect no is that no if it is equal to sum of its digits raised to nth power, where n is total no of digits in a given no.
+
+a = eval(input("enter no-"))
+no = a
+b = a
+sum = 0
+count = 0
+
+while (b > 0):  # to count no of digits in given no
+    b = b // 10
+    count += 1
+
+while (a > 0):
+    r = a % 10
+    sum += (r ** count)
+    a = a // 10
+
+if (sum == no):
+    print(no, "is armstrong no")
+else:
+    print(no, "is not a armstrong no")

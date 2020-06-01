21fb6cbad326173c464089ed4211ed07472415de
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-06-01 11:57:31 +0530

    check if given no is Neon or not

---
+++ programs/NeonNo.py
@@ -0,0 +1,15 @@
+# check if it is neon no
+
+a = eval(input("enter no-"))
+sqre = a ** 2
+sum = 0
+
+while (sqre > 0):
+    r = sqre % 10
+    sum = sum + r
+    sqre = sqre // 10
+
+if (sum == a):
+    print(a, "is neon")
+else:
+    print(a, "is not neon")

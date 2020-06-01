43e225d2d766aab6255167147dae7457dea3710b
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-06-01 11:06:39 +0530

    check if given no is prime

---
+++ programs/PrimeNo.py
@@ -0,0 +1,18 @@
+# check given no is prime or not
+import sys
+
+a = eval(input("enter no-"))
+if (a == 2):
+    print("2 is even prime no")
+    sys.exit(0)
+
+f = 0
+for i in range(2, int((a ** 0.5) + 1)):
+    if (a % i == 0):
+        f = 1
+        break
+
+if f == 0:
+    print(a, "is prime no")
+else:
+    print(a, "is not prime no")

2baa454f0c0a479db837a6a7fef81f3df316d2c3
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-06-05 12:20:33 +0530

    sort the tuple

---
+++ programs/SortTuple.py
@@ -0,0 +1,8 @@
+# sort the tuple in one line-
+
+print(tuple(sorted(list(tuple([int(i) for i in input("enter all space separated values-").split()])))))
+
+# a = tuple([int(i) for i in input("enter all space separated values-").split()])
+# b = list(a)
+# b.sort()
+# print("sorted tuple is-", tuple(b))

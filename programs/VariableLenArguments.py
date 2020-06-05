b992a67f34eb4062c381a45fd7e16e0d753273fe
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-06-05 11:48:19 +0530

    find even nos and odd nos in the variable length argument

---
+++ programs/VariableLenArguments.py
@@ -0,0 +1,16 @@
+# find even no and odd nos in variable length argument
+
+def var(*arg):
+    even = []
+    odd = []
+    for i in arg:
+        if (i % 2 == 0):
+            even.append(i)
+        else:
+            odd.append(i)
+    return even, odd
+
+
+ev, od = var(1, 2, 3, 4, 5, )
+print("even  no-", ev)
+print("odd  no-", od)

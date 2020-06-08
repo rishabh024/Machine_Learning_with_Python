5d815252740f9a81418eff9bb16fece6e8bf80a7
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-06-08 16:26:17 +0530

    use this code to check the version of different libraries

---
+++ library/CheckVersion.py
@@ -0,0 +1,17 @@
+import numpy as np
+print("numpy - ", np.__version__)
+
+import matplotlib as mp
+print("matplolib - ", mp.__version__)
+
+import pandas as p
+print("pandas - ", p.__version__)
+
+import scipy as sp
+print("scipy - ",  sp.__version__)
+
+import sklearn as sl
+print("sklearn - ", sl.__version__)
+
+import seaborn as sb
+print("seaborn - ",  sb.__version__)

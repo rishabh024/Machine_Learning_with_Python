49d32bdf325365801c93162408669b95444cbf29
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-06-10 15:11:00 +0530

    matplotlib file-3

---
+++ library/matPlotLib_Python/MatPlotLib03.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot as plt
+
+x = [1, 2, 3, 4, 5, 6]
+y = [1, 4, 9, 16, 25, 36]
+
+plt.plot(x, y, marker='o', markerfacecolor="m", markersize=10,
+              linestyle='-.', linewidth=2, label="line 1", color='g')
+
+plt.xlabel('<-------------x cordinates------------->', fontsize=14, color="g")
+plt.ylabel("<-------------y cordinates------------->", fontsize=14, color="b")
+plt.title("numbers with squares",fontsize=15, color="r")
+plt.grid(True)
+plt.legend(loc="upper right")
+plt.annotate("Square of 5", color='c', xytext=(4, 30), xy=(5, 25), arrowprops=dict( facecolor="r", shrink=.08))
+plt.axis([0, 7, 0, 37])                # in axis fnx-list is given in this way-[ x1, x2, y1, y2]
+plt.show()
+

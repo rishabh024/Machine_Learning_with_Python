e42a152b1ccef016e28208d1220c5d3e7615bd49
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-07-04 18:06:34 +0530

    mean absolute error

---
+++ ml_codes/supervised_learning_codes/6 accuracy calculation technique/17 accuracy techniques in Linear regression/2 MSE.py
@@ -0,0 +1,34 @@
+
+# shuffle is false by default = ie generation of random no is fixed = random_state = 7/seed ,       (seed =7)
+# if shuffle is true = ie generation of random no is not  fixed
+# alwasy advisable to use random data from dataset in project = eske liye we must do --> shuufle=true
+#
+# ====================================================================================================================
+# mse zoom krke dega  o/p esliye frequently use kia jata ah es method ko
+
+
+
+# MSE
+import pandas as pd
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LinearRegression
+
+filename = 'housing.csv'
+
+hnames=['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE',
+'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+
+dataframe = pd.read_csv(filename, names=hnames)
+array = dataframe.values
+
+X = array[: , 0:13]
+Y = array[:,13]
+
+kfold = KFold(n_splits=10 )
+model = LinearRegression()
+
+scoringMethod = 'neg_mean_squared_error'           #MSE
+results = cross_val_score(model, X, Y, cv=kfold, scoring=scoringMethod)
+
+print( "MSE: %.3f (%.3f)" % ( results.mean(), results.std() ) )

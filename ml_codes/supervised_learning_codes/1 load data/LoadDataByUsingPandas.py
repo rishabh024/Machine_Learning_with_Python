e71ddc20672ec4a1f5ac8c05289afbe6ffd7f185
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-07-04 18:12:13 +0530

    load online data by using Pandas

--- ml_codes/supervised_learning_codes/1 load data/LoadDataByUsingPandas.py
+++ ml_codes/supervised_learning_codes/1 load data/LoadDataByUsingPandas.py
@@ -9,7 +9,6 @@ heading_nmaes=['a','b','c','d','e','f','g''h','i']
 # to read file
 dataframe = p.read_csv(filename, names=heading_nmaes)
 
-
 # it shows only first 5 & last 5 cols of dataframe and middle cols represented by periods
 #  &  we also get list- [rows X cols] as o/p by printing dataframe.
 print('\n\n', dataframe)

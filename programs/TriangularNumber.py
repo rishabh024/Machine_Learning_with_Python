ad63c887489929a8454da9979d712b36abf20734
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-07-04 18:14:06 +0530

    TriangularNumber problem

--- programs/TriangularNumber.py
+++ programs/TriangularNumber.py
@@ -1,5 +1,5 @@
 # triangular no is the summation from 1 to the given no = total no of dots in nth triangle
-# triangular no. sequence - 1,3,6,10,15,21,28...
+# triangular no. sequence - 1,3,6,10,5 data separation(split),21,28...
 # each term of this sequence represents the no of dots in each triangular pattern
 # to get no of dots in a nth triangle,formula is -> n(n+1)/2 = (summation from 1 to nth no) = (linear summation formula)
 

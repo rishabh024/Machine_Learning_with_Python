38e84a25117d1176e108ec3258ad31cd3a750624
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-11-23 17:11:35 +0530

    assignment completed

---
+++ programs/Assignments/Assignment_04/Question_4.py
@@ -0,0 +1,9 @@
+# Developed By : Rishabh Gupta
+
+# Question is- Write a Python program to reverse a string.
+
+# Sample String : "1234abcd"
+# Expected Output : "dcba4321"
+
+a = input("enter string-")
+print("reverse of the string-", a[::-1])

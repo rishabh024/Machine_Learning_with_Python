9d02314e7e25aef8b3025a631bf241a5808a9f9b
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-11-23 17:09:32 +0530

    assignment completed

---
+++ programs/Assignments/Assignment_03/Question_6.py
@@ -0,0 +1,8 @@
+# Developed By : Rishabh Gupta
+
+# Question is-: Write a Python program to create a symmetric difference.
+
+s1 = set((1,34,  324, 12, 78, 29, 590, ))
+s2 = set((2, 9, 34, 81, 12))
+
+print("symmetric difference- ", s1 ^ s2)

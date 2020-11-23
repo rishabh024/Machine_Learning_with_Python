4ec63969b5380fe5d84ffa36345e4d1ccc614b41
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-11-23 17:08:37 +0530

    assignment completed

---
+++ programs/Assignments/Assignment_01/Question_6.py
@@ -0,0 +1,9 @@
+# Developed By : Rishabh Gupta
+
+# Question is- Write a program which prompts the user for a Fahrenheit temperature,
+# convert the temperature to Celsius and print out the converted temperature.
+
+fahr_scale = eval(input("enter the temperature in Fahrenheit -"))
+
+cel_scale = 5/9 * (fahr_scale -32)
+print("the temperature in Celsius-",  cel_scale)

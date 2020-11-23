dae6a9b4ffd29130295197f78d96cb2d45631376
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-11-23 17:10:39 +0530

    assignment completed

---
+++ programs/Assignments/Assignment_03/Question_5.py
@@ -0,0 +1,8 @@
+# Developed By : Rishabh Gupta
+
+# Question is- Write a Python program to create an intersection of sets.
+
+s1 = set((1, 23, 34, 51, 2, 9))
+s2 = set((4, 5, 2, 79, 1, 34))
+
+print("intersection_set is- ", s1 & s2)

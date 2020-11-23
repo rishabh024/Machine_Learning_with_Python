e3872939e3e9c4f98b4e105d24d901ac1c0d7f73
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-11-23 17:08:43 +0530

    assignment completed

---
+++ programs/Assignments/Assignment_01/Question_7.py
@@ -0,0 +1,11 @@
+# Developed By : Rishabh Gupta
+
+# Question is- Write a program to prompt the user for hours and rate per hour to compute gross pay.
+# Example: Enter Hours: 35
+# Enter Rate: 2.75
+# Pay: 96.25
+
+
+hr = eval(input("Enter Hours:-"))
+rate = eval(input("Enter Rate:-"))
+print("your payment is-", hr*rate)

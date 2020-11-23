4da6e985e2f5cfdaab82b5df8c1bf28226add7ae
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-11-23 17:11:20 +0530

    assignment completed

---
+++ programs/Assignments/Assignment_04/Question_1.py
@@ -0,0 +1,6 @@
+# Developed By : Rishabh Gupta
+
+# Question is- Write a Python function to find the Max of three numbers.
+
+a = list(map(int, input("enter space separated 3 nos- ").split()))
+print(" Max of three numbers-", max(a))

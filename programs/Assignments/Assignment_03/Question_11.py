5436e978c8f3c18053190b8f1727423b5817dd1a
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-11-23 17:11:11 +0530

    assignment completed

---
+++ programs/Assignments/Assignment_03/Question_11.py
@@ -0,0 +1,22 @@
+# Developed By : Rishabh Gupta
+
+# Question is- Write a Python program to count the frequency of words in a file.
+
+filename = "Dataset.txt"
+no_of_words = 0
+#
+# while line:
+#     with open(filename) as f1:
+#         line = f1.readline()
+#         co = line.split()
+#         print(len(co))
+#
+# # print("wrds torl - ", no_of_words )
+#
+#
+#
+#
+#
+#
+#
+#

7075fdcbac4373f744a767cf62bf883f66d88651
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-11-23 17:11:25 +0530

    assignment completed

---
+++ programs/Assignments/Assignment_04/Question_2.py
@@ -0,0 +1,9 @@
+# Developed By : Rishabh Gupta
+
+# Question is-  Write a Python function to sum all the numbers in a list.
+
+# Sample List : (8, 2, 3, 0, 7)
+# Expected Output : 20
+
+a =list(map(int, input("enter space separated nos-").split()))
+print("sum all the numbers in a list-", sum(a))

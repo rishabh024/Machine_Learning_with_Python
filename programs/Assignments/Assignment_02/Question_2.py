94cb68f93e4c4719122de80cacbf5ceb6b305e47
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-11-23 17:09:05 +0530

    assignment completed

---
+++ programs/Assignments/Assignment_02/Question_2.py
@@ -0,0 +1,7 @@
+# Developed By : Rishabh Gupta
+
+# Question is- Write a Python program to get the largest number from a list.
+
+
+a = list(map(int, input("enter the space separated nos-").split()))
+print("The largest number from a list is-", max(a))

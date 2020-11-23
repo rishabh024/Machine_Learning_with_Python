ed5cbd9cec6d9da5a990dbab26ebe54b96929130
Rishabh Gupta <rishabh.gupta_cs18@gla.ac.in>
2020-11-23 17:10:46 +0530

    assignment completed

---
+++ programs/Assignments/Assignment_03/Question_7.py
@@ -0,0 +1,11 @@
+# Developed By : Rishabh Gupta
+
+# Question is- Write a Python program to create a union of sets.
+
+# Note- set fnc will take at most 1 arg-
+# s1 = set(1,22, 19, 38, 43)      # error
+
+s1 = set((1,22, 19, 38, 43) )
+s2 = set((0, 3,2))
+
+print("union of sets- ", s1 | s2)
